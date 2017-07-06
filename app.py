cf2579202571d8e5e6c57a352adf0d4d95857a95
sshaikh2 <sshaikh2@ncsu.edu>
2017-07-06 19:00:23 -0400

    Added basic HTML and Bootstrap CSS

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import os
 import signal
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 from movies import generator
 
 app = Flask(__name__)
+Bootstrap(app)
 
 signal.signal(signal.SIGINT, lambda s, f:os._exit(0))
 
 @app.route("/")
 def generate_movie_review():
-	page = '<html><body><h2>'
-	page += generator.generate_movie_review();
-	page += '</h2></body></html>'
-	return page
+	result = generator.generate_movie_review()
+	return render_template('index.html', review = result)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port = os.getenv('PORT'))
