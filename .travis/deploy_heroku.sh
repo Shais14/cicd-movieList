b5300d8e4a66bed3db3725446859bfa57213f0a7
sshaikh2 <sshaikh2@ncsu.edu>
2017-07-06 16:52:05 -0400

    fixing deployment bug

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,5 +2,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 #heroku container:login
-docker login -u _ -p $HEROKU_API_KEY registry.heroku.com
+docker login -u _ -p=$(heroku auth:token) registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
